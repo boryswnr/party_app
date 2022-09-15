431779c8eba2ed363b66aac34d7c723ff46d39c5
boryswnr <borysweiner@gmail.com>
2022-09-15 22:19:16 +0200

    set up django proeject and api app in it; created room model, RoomSerializer, api endpoint

---
+++ partyapp/api/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from rest_framework import generics
+from .serializer import RoomSerializer
+from .models import Room
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
