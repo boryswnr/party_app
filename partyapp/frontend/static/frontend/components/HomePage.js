a7bfc5d8056a7ec2f3e76ad861c9dfd296306114
boryswnr <borysweiner@gmail.com>
2022-09-27 13:53:59 +0200

    update gitignore

--- partyapp/frontend/static/frontend/components/HomePage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function HomePage() {
-    return React.createElement("h1", null, "This is HomePage");
-}
-export default HomePage;
--- partyapp/frontend/static/frontend/components/JoinRoom.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const JoinRoom = () => {
-    return (React.createElement(React.Fragment, null,
-        React.createElement("h1", null, "This is join room page")));
-};
-export default JoinRoom;
