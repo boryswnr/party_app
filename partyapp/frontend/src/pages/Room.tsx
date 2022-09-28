a7ffea6a4b1bca78c20e615feac44ee64fc4ec03
boryswnr <borysweiner@gmail.com>
2022-09-28 17:53:25 +0200

    added 'leave room' functionality

--- partyapp/frontend/src/pages/Room.tsx
+++ partyapp/frontend/src/pages/Room.tsx
@@ -1,15 +1,23 @@
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Room = () => {
     const { roomCode } = useParams();
     const [votesToSkip, setVotesToSkip] = useState(2);
     const [guestCanPause, setGuestCanPause] = useState(true);
     const [isHost, setIsHost] = useState(false);
+    const navigate = useNavigate();
 
     const getRoomDetails = () => {
         fetch("/api/get-room" + "?code=" + roomCode)
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) {
+                    navigate("/");
+                }
+                return response.json();
+            })
             .then((data) => {
                 setVotesToSkip(data.votes_to_skip);
                 setGuestCanPause(data.guest_can_pause);
@@ -21,14 +29,43 @@ const Room = () => {
         getRoomDetails();
     }, []);
 
+    const leaveBtnPressed = () => {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        };
+        fetch("/api/leave-room", requestOptions).then((_response) => {
+            navigate("/");
+        });
+    };
+
     return (
-        <div>
-            <h3>{roomCode}</h3>
-            <p>This is Room component</p>
-            <p>Votes to skip: {votesToSkip}</p>
-            <p>Guests can pause: {guestCanPause ? "Yes" : " No"}</p>
-            <p>Host: {isHost ? "Yes" : " No"}</p>
-        </div>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                textAlign: "center",
+            }}
+        >
+            <Typography variant="h4">Code: {roomCode}</Typography>
+
+            <Typography variant="h6">Votes to skip: {votesToSkip}</Typography>
+
+            <Typography variant="h6">
+                Guests can pause: {guestCanPause ? "Yes" : " No"}
+            </Typography>
+
+            <Typography variant="h6">Host: {isHost ? "Yes" : " No"}</Typography>
+
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => leaveBtnPressed()}
+            >
+                Leave room
+            </Button>
+        </Box>
     );
 };
 
