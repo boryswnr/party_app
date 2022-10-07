c3f0c3c8ea13337904ded2ce28896579e6c4f80b
boryswnr <borysweiner@gmail.com>
2022-10-07 17:46:05 +0200

    setup spotify API backend

--- partyapp/frontend/urls.py
+++ partyapp/frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index),
+    path('', index, name=''),
     path('create', index),
     path('join', index),
     path('room/<str:roomCode>', index),
