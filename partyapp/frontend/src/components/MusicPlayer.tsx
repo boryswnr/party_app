a3e6760db3f3697eaefb19a0c8ce579f9d55898d
boryswnr <borysweiner@gmail.com>
2022-10-11 09:22:21 +0200

    edited styling of music player

--- partyapp/frontend/src/components/MusicPlayer.tsx
+++ partyapp/frontend/src/components/MusicPlayer.tsx
@@ -32,19 +32,34 @@ const MusicPlayer = ({
 
     return (
         <Card>
-            <Box sx={{ textAlign: "center" }}>
-                <img src={image_url} height="100%" width="100%" />
-                <Typography variant="h5">{title}</Typography>
-                <Typography color="textSecondary" variant="subtitle1">
-                    {artist}
-                </Typography>
-                <Box>
-                    <IconButton>
-                        {is_playing ? <Pause /> : <PlayArrow />}
-                    </IconButton>
-                    <IconButton>
-                        <SkipNext />
-                    </IconButton>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
+            >
+                <Box sx={{ maxWidth: "40%" }}>
+                    <img src={image_url} height="100%" width="100%" />
+                </Box>
+                <Box
+                    sx={{
+                        width: "60%",
+                    }}
+                >
+                    <Typography variant="h5">{title}</Typography>
+                    <Typography color="textSecondary" variant="subtitle1">
+                        {artist}
+                    </Typography>
+                    <Box>
+                        <IconButton>
+                            {is_playing ? <Pause /> : <PlayArrow />}
+                        </IconButton>
+                        <IconButton>
+                            <SkipNext />
+                        </IconButton>
+                    </Box>
                 </Box>
             </Box>
             <LinearProgress variant="determinate" value={songProgress} />
